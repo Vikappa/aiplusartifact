1f5b5dfcba92e7b31240079b9e9d8ee8e78e20c2
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-04-30 19:31:41 +0200

    Initial commit

---
+++ src/main/java/com/aiplus/aiplus/AiplusApplication.java
@@ -0,0 +1,13 @@
+package com.aiplus.aiplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiplusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aiplus/aiplus/AiplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiplus.aiplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
