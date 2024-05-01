6a0aa9b1fc70c9e80e5cecf9484ff592120b043d
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-05-01 02:12:26 +0200

    Database connection set up

--- src/main/java/com/aiplus/aiplus/AiplusApplication.java
+++ src/main/java/com/aiplus/aiplus/AiplusApplication.java
@@ -1,5 +1,8 @@
 package com.aiplus.aiplus;
-
+import com.cloudinary.*;
+import com.cloudinary.utils.ObjectUtils;
+import io.github.cdimascio.dotenv.Dotenv;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
