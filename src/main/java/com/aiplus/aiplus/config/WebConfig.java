c85c3e450524b5f720f2f7c62fda42247415dad6
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-05-04 20:27:49 +0200

    Added web configuration

---
+++ src/main/java/com/aiplus/aiplus/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.aiplus.aiplus.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // Permette CORS su tutte le rotte
+                .allowedOrigins("http://localhost:3001")  // URL del tuo frontend React
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
