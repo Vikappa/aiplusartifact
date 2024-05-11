96208b304cd6af8f5ab2aff50c48ce1b7ec8996d
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-05-11 15:27:41 +0200

    Added entities, get User by token controller added

--- src/main/java/com/aiplus/aiplus/services/UserService.java
+++ src/main/java/com/aiplus/aiplus/services/UserService.java
@@ -2,11 +2,19 @@ package com.aiplus.aiplus.services;
 
 import com.aiplus.aiplus.entities.users.User;
 import com.aiplus.aiplus.repositories.UserDAO;
+import com.aiplus.aiplus.security.JWTTools;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.UUID;
 
 @Service
 public class UserService {
+
+    @Autowired
+    JWTTools jwtTools;
 
     @Autowired
     UserDAO userDAO;
@@ -14,4 +22,14 @@ public class UserService {
     public User addUser(User user) {
         return userDAO.save(user);
     }
+
+    public User getUserByToken(String token) {
+
+        UUID userID = jwtTools.extractUserUUID(token);
+        userDAO.findById(userID).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        return userDAO.findByUUID(userID);
+
+    }
+
 }
