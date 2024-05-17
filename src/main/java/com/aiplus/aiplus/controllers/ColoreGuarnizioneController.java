8c5b1840c0f971a7a85dc997e4b30b95f7cf82c0
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-05-17 13:39:25 +0200

    Colours controller fix

--- src/main/java/com/aiplus/aiplus/controllers/ColoreGuarnizioneController.java
+++ src/main/java/com/aiplus/aiplus/controllers/ColoreGuarnizioneController.java
@@ -4,6 +4,7 @@ import com.aiplus.aiplus.entities.stockentities.ColoreGuarnizione;
 import com.aiplus.aiplus.services.ColoreGuarnizioneService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +17,7 @@ public class ColoreGuarnizioneController {
     @Autowired
     private ColoreGuarnizioneService coloreGuarnizioneService;
 
-
-    @RequestMapping("/getAll")
+    @GetMapping("/getall")
     public ResponseEntity<List<ColoreGuarnizione>> getAll() {
         return coloreGuarnizioneService.getAll();
     }
