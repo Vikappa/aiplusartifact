2604e16ce09b0aff57ca4acd8a65f637bbe66d27
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-05-28 15:35:24 +0200

    Update webconfig

--- src/main/java/com/aiplus/aiplus/config/WebConfig.java
+++ src/main/java/com/aiplus/aiplus/config/WebConfig.java
@@ -1,15 +1,19 @@
 package com.aiplus.aiplus.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${allowed.origins}")
+    private String[] allowedOrigins;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://aipluswebinterface.vercel.app","https://aipluswebinterface-f5ledl3ig-vikappas-projects.vercel.app/")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
