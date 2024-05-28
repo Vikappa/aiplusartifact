2cf7791a9ecfbf7c92deb311aa19f4c2acb5c27f
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-05-28 23:47:09 +0200

    Added data ora preparazione

--- src/main/java/com/aiplus/aiplus/services/RicettaService.java
+++ src/main/java/com/aiplus/aiplus/services/RicettaService.java
@@ -50,6 +50,9 @@ public class RicettaService {
 
     @Transactional
     public Ricetta createNewRicetta(NewRicetta newRicetta) {
+        if(!ricettaDAO.findByName(newRicetta.name())) {
+            throw new RuntimeException("Ricetta already exists");
+        }
         logger.info(newRicetta.name());
         logger.info("Creo la ricetta:", newRicetta.name());
 
