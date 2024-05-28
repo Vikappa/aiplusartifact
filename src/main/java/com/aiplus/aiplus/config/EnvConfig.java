36dec8f6b506b2c47a85671534e5fc4f43cfc048
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-05-28 03:45:24 +0200

    fix  application.properties EnvConfig

--- src/main/java/com/aiplus/aiplus/config/EnvConfig.java
+++ src/main/java/com/aiplus/aiplus/config/EnvConfig.java
@@ -4,8 +4,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource(value = "file:./env.properties")
+@PropertySource(value = "file:./env.properties", ignoreResourceNotFound = true)
 public class EnvConfig {
+
 
 
 
