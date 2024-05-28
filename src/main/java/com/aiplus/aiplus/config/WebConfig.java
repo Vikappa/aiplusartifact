f20ed84057efd6a5485aaf861eac63c069371cd6
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-05-28 15:16:54 +0200

    Update webconfig

--- src/main/java/com/aiplus/aiplus/config/WebConfig.java
+++ src/main/java/com/aiplus/aiplus/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://aipluswebinterface.vercel.app")
+                .allowedOrigins("http://localhost:5173", "https://aipluswebinterface.vercel.app","https://aipluswebinterface-f5ledl3ig-vikappas-projects.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
